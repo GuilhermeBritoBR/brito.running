80087d7619860ced14a1cec7e047b64d40617462
Guilherme Santiago de Brito <125681424+GuilhermeBritoBR@users.noreply.github.com>
2023-06-10 21:35:27 -0300

    1.3

---
+++ scripts/carrosel.js
@@ -0,0 +1,6 @@
+function insta()   {
+    window.location.assign("https://www.instagram.com/brito.running/");
+}
+function face()   {
+    window.location.assign("https://www.facebook.com/profile.php?id=100093162848662");
+}
