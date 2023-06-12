632b48b425a251c4c237a9083a36664f42c1a882
Guilherme Santiago de Brito <125681424+GuilhermeBritoBR@users.noreply.github.com>
2023-06-12 19:35:12 -0300

    1.6
    -contact finish;
    -grays filter in header and hover;
    -ortografy correction:

--- scripts/carrosel.js
+++ scripts/carrosel.js
@@ -5,5 +5,5 @@ function youtube()   {
     window.location.assign("https://www.youtube.com/@brito.running");
 }
 function button()   {
-    window.location.assign("https://guilhermebritobr.github.io/brito.running/");
+    window.location.assign("index.html");
 }
