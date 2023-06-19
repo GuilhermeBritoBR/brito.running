d59ef9496ced238637271efcd3e2f79c23567561
Guilherme Santiago de Brito <125681424+GuilhermeBritoBR@users.noreply.github.com>
2023-06-18 21:09:43 -0300

    1.7

--- scripts/carrosel.js
+++ scripts/carrosel.js
@@ -2,7 +2,7 @@ function insta()   {
     window.location.assign("https://www.instagram.com/brito.running/");
 }
 function youtube()   {
-    window.location.assign("https://www.youtube.com/@brito.running");
+    window.location.assign("https://www.strava.com/athletes/85524343");
 }
 function button()   {
     window.location.assign("index.html");
